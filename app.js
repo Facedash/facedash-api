5ceb13e8989289b5f964c819c328915dd242d932
Abdelatif Sebbane <sebbane.a@gmail.com>
2014-06-07 15:59:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -56,11 +56,13 @@ app.use(app.router);
 /**
  * General.
  */
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+app.all('/*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
   next();
- });
+});
 
 app.get('/', routes.index);
 
